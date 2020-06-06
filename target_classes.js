d1bcba58f94442b7653c192a530b35033ac50a41
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2020-06-05 21:48:17 -0400

    some changes

--- target_classes.js
+++ target_classes.js
@@ -1,4 +1,9 @@
+//https://lancaster.unl.edu/pest/resources/339_beneficialbugs.pdf
+//beneficial insects resource
+
 TARGET_CLASSES = {
-    0: "Normal",
-    1: "Disease"
+    0: "Locust (Schistocerca gregaria) - Harmful",
+    1: "Armyworm (Spodoptera frugiperda) - Harmful",
+    2: "Ladybird Beetle (Coccinellidae) - Beneficial",
+    3: "Honeybee (Apis mellifera) - Beneficial",
 };

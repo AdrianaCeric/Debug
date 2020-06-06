24036988abec335ae48d44befbcbebd68f6046a7
Pop0097 <dhruvrawat09@gmail.com>
2020-06-06 18:57:00 -0400

    Update to predict.js

--- js/predict.js
+++ js/predict.js
@@ -18,45 +18,54 @@ $(async function() {
     maxPredictions = model.getTotalClasses();
 
     $("#predict-button").click(async function() {
-
+        $(".predict").show();
         $(".progress").html("<h5>Classifying bug...</h5>");
         var prediction = await model.predict(document.getElementById("selected-image"));
+        var fails = 0;
 
         console.log(prediction)
         for (var i = 0; i < prediction.length; i++) {
-            console.log(prediction[i.toString()])
+            console.log(prediction[i.toString()]);
             if (prediction[i.toString()].probability > 0.6) {
-                console.log("ITS THISSSS")
-                    //laeh
+                console.log("ITS THISSSS");
+
                 if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "L") {
-                    setP("Locust (Schistocerca gregaria) - Harmful", 0, "https://en.wikipedia.org/wiki/Desert_locust");
+                    setP("Locust (Schistocerca gregaria) - Harmful", 1, "https://en.wikipedia.org/wiki/Desert_locust");
                 } else if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "A") {
-                    setP("Armyworm (Spodoptera frugiperda) - Harmful", 0, "https://en.wikipedia.org/wiki/Fall_armyworm");
+                    setP("Armyworm (Spodoptera frugiperda) - Harmful", 1, "https://en.wikipedia.org/wiki/Fall_armyworm");
                 } else if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "E") {
-                    setP("Earwig (Dermaptera) - Beneficial", 1, "https://en.wikipedia.org/wiki/Earwig");
+                    setP("Earwig (Dermaptera) - Beneficial", 2, "https://en.wikipedia.org/wiki/Earwig");
                 } else if (prediction[i.toString()].probability >= 0.6 && prediction[i.toString()].className.toString().charAt(0) == "H") {
-                    setP("Honeybee (Apis mellifera) - Beneficial", 1, "https://en.wikipedia.org/wiki/Western_honey_bee");
-                } else {
-                    setP("Unable to identify. Please try again with another photo.", 2, "https://en.wikipedia.org/wiki/Western_honey_bee");
+                    setP("Honeybee (Apis mellifera) - Beneficial", 2, "https://en.wikipedia.org/wiki/Western_honey_bee");
                 }
                 //$("#prediction-list").html(prediction[i.toString()].className + "<br>Accuracy:" + prediction[i.toString()].probability * 100 + "%");
+            } else {
+                fails++;
             }
+        }
+        console.log(fails)
+        if (fails == 4) {
+            setP("Unable to identify. Please try again with another photo.", 3, "https://en.wikipedia.org/wiki/Western_honey_bee");
         }
     })
 })
 
+
 function setP(a, isgood, link) {
-    if (isgood != 2) {
-        $("#prediction-list").html(a + ` \n<a href="${link}">Learn more</a>`);
+    if (isgood != 3) {
+        $("#prediction-list").html(a + '<p><a href = "' + link + `"> Learn more </p>`);
+    } else {
+        $("#prediction-list").html(a);
     }
-    if (isgood == 1) {
+
+    if (isgood == 2) {
         $("#prediction-list").addClass("good");
         $("#prediction-list").removeClass("bad");
-    } else if (isgood == 0) {
+    } else if (isgood == 1) {
         $("#prediction-list").addClass("bad");
         $("#prediction-list").removeClass("good");
     } else {
-        $("#prediction-list").removeClass("bad");
         $("#prediction-list").removeClass("good");
+        $("#prediction-list").removeClass("bad");
     }
 }

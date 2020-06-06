2ad9189b64c33598a68e188d4302ad63560427f3
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2020-06-06 10:55:07 -0400

    changes

--- predict.js
+++ predict.js
@@ -1,48 +1,109 @@
-$("#image-selector").change(function() {
-    let reader = new FileReader();
-    reader.onload = function() {
-        let dataURL = reader.result;
-        $("#selected-image").attr("src", dataURL);
-        $("#prediction-list").empty();
-    }
+$(function() {
+    $("#image-selector").change(function() {
+        let reader = new FileReader();
+        reader.onload = function() {
+            let dataURL = reader.result;
+            $("#selected-image").attr("src", dataURL);
+            $("#prediction-list").empty();
+        }
 
-    let file = $("#image-selector").prop('files')[0];
-    reader.readAsDataURL(file);
-});
-
-let model;
-$(document).ready(async function() {
-    $('.progress-bar').show();
-    console.log("Loading model...");
-    model = await tf.loadGraphModel('model/model.json');
-    console.log("Model loaded.");
-    $('.progress-bar').hide();
-});
-
-$("#predict-button").click(async function() {
-    let image = $('#selected-image').get(0);
-
-    // Pre-process the image
-    console.log("Loading image...");
-    let tensor = tf.browser.fromPixels(image, 3)
-        .resizeNearestNeighbor([224, 224]) // change the image size
-        .expandDims()
-        .toFloat()
-        .reverse(-1); // RGB -> BGR
-    let predictions = await model.predict(tensor).data();
-    console.log(predictions);
-    let top5 = Array.from(predictions)
-        .map(function(p, i) { // this is Array.map
-            return {
-                probability: p,
-                className: TARGET_CLASSES[i] // we are selecting the value from the obj
-            };
-        }).sort(function(a, b) {
-            return b.probability - a.probability;
-        }).slice(0, 2);
-
-    $("#prediction-list").empty();
-    top5.forEach(function(p) {
-        $("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+        let file = $("#image-selector").prop('files')[0];
+        reader.readAsDataURL(file);
     });
-});
+
+
+
+
+    classifier = ml5.imageClassifier("put the teachable machine model link here", function() {
+
+        $(".progress").html("Attempting to classify...");
+
+        $("#predict-button").click(function() {
+            classifier.classify(document.getElementById("selected-image"), function(err, result) {
+                $(".progress").hide();
+                if (err) {
+                    console.log("a oof occured");
+                } else {
+
+                    if (results[0].confidence >= 0.6) {
+                        //YAY WE FOUND SOMETHING
+
+                        var bug = results[0].label;
+                        $("#prediction-list").html(bug);
+
+
+                    } else {
+                        //NO MATCH DETECTED
+                        var bug = false;
+                        $("#prediction-list").html("Classification failed!");
+                    }
+                }
+
+
+
+            })
+
+        })
+
+
+
+
+
+
+
+
+
+
+    })
+
+})
+
+
+// $("#image-selector").change(function() {
+//     let reader = new FileReader();
+//     reader.onload = function() {
+//         let dataURL = reader.result;
+//         $("#selected-image").attr("src", dataURL);
+//         $("#prediction-list").empty();
+//     }
+
+//     let file = $("#image-selector").prop('files')[0];
+//     reader.readAsDataURL(file);
+// });
+
+// let model;
+// $(document).ready(async function() {
+//     $('.progress-bar').show();
+//     console.log("Loading model...");
+//     model = await tf.loadGraphModel('model/model.json');
+//     console.log("Model loaded.");
+//     $('.progress-bar').hide();
+// });
+
+// $("#predict-button").click(async function() {
+//     let image = $('#selected-image').get(0);
+
+//     // Pre-process the image
+//     console.log("Loading image...");
+//     let tensor = tf.browser.fromPixels(image, 3)
+//         .resizeNearestNeighbor([224, 224]) // change the image size
+//         .expandDims()
+//         .toFloat()
+//         .reverse(-1); // RGB -> BGR
+//     let predictions = await model.predict(tensor).data();
+//     console.log(predictions);
+//     let top5 = Array.from(predictions)
+//         .map(function(p, i) { // this is Array.map
+//             return {
+//                 probability: p,
+//                 className: TARGET_CLASSES[i] // we are selecting the value from the obj
+//             };
+//         }).sort(function(a, b) {
+//             return b.probability - a.probability;
+//         }).slice(0, 2);
+
+//     $("#prediction-list").empty();
+//     top5.forEach(function(p) {
+//         $("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+//     });
+// });
